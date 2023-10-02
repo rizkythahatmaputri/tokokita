7dfd0c0d370f24e0631d3ac0b1088292692080b8
rizkythahatmaputri <rithakendal@gmail.com>
2023-10-02 18:59:19 +0700

    Initial commit

---
+++ lib/ui/about_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Ini adalah project flutter milik:'),
+          Text('Rizkytha Hatma'),
+          Text('H1D021044'),
+        ],
+      )),
+    );
+  }
+}
